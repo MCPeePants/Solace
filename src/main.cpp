b2e3130ec01e9a9dd476c5ff1e3ba24485cb04f8
Chris Latham <BackwardSpy@googlemail.com>
2010-05-30 21:38:23 +0100

    Merged Raccoon's new main.cpp with mine, and modified RenderScene so it now takes a reference to the window rather than a pointer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 
 /// Function declarations ///
-void renderScene(sf::RenderWindow*);
+void renderScene(sf::RenderWindow&);
+void getEvents(sf::RenderWindow&);
 /// ///
 
 // Window width and height
@@ -21,15 +22,9 @@ int main()
     while(Wnd.IsOpened())
     {
         // Process window events
-        sf::Event event;
-        while(Wnd.GetEvent(event))
-        {
-            if(event.Type == sf::Event::Closed)
-                Wnd.Close();
-        }
-
+        getEvents(Wnd);
         // Render the game onto the current window
-        renderScene(&Wnd);
+        renderScene(Wnd);
     }
 
     return 0;
@@ -39,9 +34,39 @@ int main()
 /// If possible, do ALL drawing code in here, not
 /// outside of it.
 /// ARGUMENTS:
-///     *Surface => Pointer to the window to draw on
-void renderScene(sf::RenderWindow *surface)
+///     *Surface => Reference of the context to draw on
+void renderScene(sf::RenderWindow& surface)
 {
-    surface->Clear();
-    surface->Display();
+    surface.Clear();
+    surface.Display();
+}
+
+/// This function handles any Events/Inputs thrown to the game
+/// Do any input code here
+/// Arguments are window => Window to register inputs from
+/// THANKS TO JA_COP FOR HELPING ME OUT WITH THIS :)
+void getEvents(sf::RenderWindow& window)
+{
+	sf::Event event;
+	while(window.GetEvent(event))
+	{
+		switch(event.Type)
+		{
+		case sf::Event::Closed:
+			window.Close();
+			break;
+
+		case sf::Event::KeyPressed:
+			if(event.Key.Code == sf::Key::Escape)
+				window.Close();
+			break;
+
+		default:
+			// I don't know what to do here but ja_cop says this:
+			// Pass to input module or GUI module (placeholder)
+			// so do that i guess
+            // TODO: ADD MORE INPUTS
+			break;
+		}
+	}
 }
