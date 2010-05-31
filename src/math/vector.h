f1d143b3eb4b2b5452172ef66757f4b2aa34cf30
z33ky <z33ky@mobileblahbuntu.fritz.box>
2010-05-31 14:28:28 +0200

    fixed formatting added inline-file (due to templated class) usage of additional template parameters added distance-functions added lendthSqr-function fixed set-function fixed setLength-function note: untested

--- src/math/vector.h
+++ src/math/vector.h
@@ -3,54 +3,87 @@
 
 #include <cmath>
 
-namespace math
-{
-    template<typename T>
-    class Vector
-    {
-        public:
+#include "math/vector_fwd.h"
 
-        Vector();
-        Vector(T _x, T _y);
-        ~Vector();
+namespace math{
 
+  // note: there's some sweet Boost class you can extend to autoamtically create a bunch of operators
+  //  look into it!
+  template<typename T>
+  class Vector
+  {
+    public:
 
-        void operator=(const Vector<T> &a);
+    Vector();
+    Vector(T x, T y);
+    ~Vector();
 
-        // assignments
-        void operator=(const Vector<T> &a);
-        void operator+=(const Vector<T> &a);
-        void operator-=(const Vector<T> &a);
-        void operator*=(T a);
-        void operator/=(T a);
+    // assignments
+    template<typename T2>
+    void operator=(const Vector<T2> &a){ tx = a.getX(); ty = a.getY();}
+    template<typename T2>
+    void operator+=(const Vector<T2> &a){ tx += a.getX(); ty += a.getY();}
+    template<typename T2>
+    void operator-=(const Vector<T2> &a){ tx -= a.getX(); ty -= a.getY();}
+    template<typename T2>
+    void operator*=(T2 a){tx *= a; ty *= a;}
+    // FIXME: dividing a number from a vector sounds weird to me ;z33ky
+    template<typename T2>
+    void operator/=(T2 a){tx /= a; ty /= a;}
 
-        void normalize();
-        void setLength(T l);
-        T length();
+    void normalize(){ setLength<T>(1); }
+    // FIXME: should those two length-method not always use a floating-point type? ;z33ky
+    template<typename T2>
+    void setLength(T2 len);
+    T lengthSqr(){ return x * x + y * y;}
+    T length(){ return std::sqrt(lengthSqr());}
 
-        T getX();
-        T getY();
-        void set(T, T);
+    template<typename T2>
+    T distanceSqr(const Vector<T2> &a){ return ((*this) - a).lengthSqr();}
+    template<typename T2>
+    T distance(const Vector<T2> &a){ return std::sqrt(distanceSqr());}
 
+    T getX() const{ return tx;}
+    void setX(T x){ tx = x;}
+    T getY() const{ return ty;}
+    void setY(T y){ ty = y;}
 
+    void set(T, T){ tx = x; ty = y;}
 
-        private:
-        T tx;
-        T ty;
-    };
-    // addition, subtraction and negation
+    private:
+    T tx;
+    T ty;
+  };
 
-    template<typename T> Vector<T> operator+(const Vector<T> &a, const Vector<T> &b);
-    template<typename T> Vector<T> operator-(const Vector<T> &a, const Vector<T> &b);
-    template<typename T> Vector<T> operator-(const Vector<T> &a);
-    // multiplication
-    template<typename T> Vector<T> operator*(const Vector<T> &a, T f);
-    template<typename T> Vector<T> operator*(T f, const Vector<T> &a);
-    // division
-    template<typename T> Vector<T> operator/(const Vector<T> &a, T f);
-    template<typename T> Vector<T> operator/(T f, const Vector<T> &a);
-    // dot
-    template<typename T> T operator*(const Vector<T> &a, const Vector<T> &b);
+  // addition, subtraction and negation
+  template<typename T, typename T2>
+  Vector<T> operator+(const Vector<T> &a, const Vector<T2> &b);
+
+  template<typename T, typename T2>
+  Vector<T> operator-(const Vector<T> &a, const Vector<T2> &b);
+
+  template<typename T>
+  Vector<T> operator-(const Vector<T> &a);
+
+  // multiplication
+  template<typename T, typename T2>
+  Vector<T> operator*(const Vector<T> &a, T2 f);
+
+  template<typename T, typename T2>
+  Vector<T> operator*(T2 f, const Vector<T> &a);
+
+  // division
+  template<typename T, typename T2>
+  Vector<T> operator/(const Vector<T>&a, T2 f);
+
+  template<typename T, typename T2>
+  Vector<T> operator/(T2 f, const Vector<T> &a);
+
+  // dot
+  template<typename T, typename T2>
+  T operator*(const Vector<T> &a, const Vector<T2> &b);
+
+  #include "vector.inl"
 
 }
 #endif
---
+++ src/math/vector.inl
@@ -0,0 +1,67 @@
+template<typename T>
+Vector<T>::Vector(){}
+
+template<typename T>
+Vector<T>::Vector(T x, T y)
+  :tx(x), ty(y){}
+
+template<typename T>
+Vector<T>::~Vector(){}
+
+template<typename T> template<typename T2>
+void Vector<T>::setLength(T2 len)
+{
+  T scale = len * len / lengthSqr();
+  tx *= scale;
+  ty *= scale;
+}
+
+template<typename T, typename T2>
+Vector<T> operator+(const Vector<T> &a, const Vector<T2> &b)
+{
+  return Vector<T>(a.getX()+b.getX(), a.getY()+b.getY());
+}
+
+template<typename T, typename T2>
+Vector<T> operator-(const Vector<T> &a, const Vector<T2> &b)
+{
+  return Vector<T>(a.getX()-b.getX(), a.getY()-b.getY());
+}
+
+template<typename T>
+Vector<T> operator-(const Vector<T> &a)
+{
+  return Vector<T>(-a.getX(), -a.getY());
+}
+
+// operator*(const Vector<T> &a, T f);
+
+template<typename T, typename T2>
+Vector<T> operator*(const Vector<T> &a, T2 f)
+{
+  return Vector<T>(a.getX()*f, a.getY()*f);
+}
+
+template<typename T, typename T2>
+Vector<T> operator*(T2 f, const Vector<T> &a)
+{
+  return Vector<T>(a.getX()*f, a.getY()*f);
+}
+
+template<typename T, typename T2>
+Vector<T> operator/(const Vector<T> &a, T2 f)
+{
+  return Vector<T>(a.getX()/f, a.getY()/f);
+}
+
+template<typename T, typename T2>
+Vector<T> operator/(T2 f, const Vector<T> &a)
+{
+  return Vector<T>(a.getX()/f, a.getY()/f);
+}
+
+template<typename T, typename T2>
+T operator*(const Vector<T> &a, const Vector<T2> &b)
+{
+  return (a.getX()*b.getX() + a.getY()*b.getY());
+}
