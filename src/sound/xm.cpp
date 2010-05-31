eb235a46144e086986495a4d3a1b3d9668a487a9
Esa Lakaniemi <esalaka@gmail.com>
2010-05-31 22:58:02 +0300

    Added sound::XmSound. No guarantee that works.
    TODO: Check that XmSound actually works. Hurr. In theory it should.

---
+++ src/sound/xm.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "xm.h"
+
+namespace sound{
+
+  bool XmSound::Load(std::string fileName)
+  {
+
+    stat res;
+    fstream input;
+    char * buffer;
+
+    if((stat(fileName.c_str(), &res) != 0))
+      return false;
+
+    samp_sz = res.st_size;
+    buffer = new char[samp_sz + 1]; // + 1 just in case...
+
+    input.open( fileName, std::ios::in | std::ios::binary );
+    if(!input.read(buffer, res.st_size))
+      return false;
+
+    mpFile = *ModPlug_Load(buffer, res.st_size);
+    samples = new signed short int[(samp_sz / 2) + 1]; // Total size should be the same as with the char buffer
+
+    ModPlug_Read( &mpFile, samples, samp_sz );
+
+    return true;
+
+  }
+
+  Samples XmSound::GetSamples()
+  {
+    Samples samp;
+
+    samp.samples = samples;
+    samp.sz = samp_sz;
+  }
+
+}
