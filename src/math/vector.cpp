75abdec2b530a4e4a72b2f35dc6a878df084925b
Matt Shepherd <mistagiggles@googlemail.com>
2010-05-31 12:17:15 +0100

    Vector class written, using templates

--- src/math/vector.cpp
+++ src/math/vector.cpp
@@ -2,4 +2,39 @@
 
 namespace math
 {
+    template<typename T> Vector<T>::Vector(){};
+    template<typename T> Vector<T>::Vector(T _x, T _y){x=_x; y=_y;}
+    template<typename T> Vector<T>::~Vector(){};
+
+    template<typename T> T Vector<T>::x() {return x;}
+    template<typename T> T Vector<T>::y() {return y;}
+
+    template<typename T> void Vector<T>::set(T _x,T _y) { tx = _x; ty = _y;}
+
+    // assignments
+    template<typename T> void Vector<T>::operator=(const Vector<T> &a) {tx = a.x(); ty = a.y();}
+    template<typename T> void Vector<T>::operator+=(const Vector<T> &a){tx += a.x(); ty += a.y();}
+    template<typename T> void Vector<T>::operator-=(const Vector<T> &a){tx -= a.x(); ty -= a.y();}
+    template<typename T> void Vector<T>::operator*=(T a){tx *= a; ty *= a;}
+    template<typename T> void Vector<T>::operator/=(T a){tx /= a; ty /= a;}
+
+
+    template<typename T> Vector<T> operator+(const Vector<T> &a, const Vector<T> &b) { return Vector<T>(a.x()+b.x(), a.y()+b.y());}
+    template<typename T> Vector<T> operator-(const Vector<T> &a, const Vector<T> &b) { return Vector<T>(a.x()-b.x(), a.y()-b.y());}
+
+    template<typename T> Vector<T> operator-(const Vector<T> &a) { return Vector<T>(-a.x(), -a.y());}
+
+    // operator*(const Vector<T> &a, T f);
+
+    template<typename T> Vector<T> operator*(const Vector<T> &a, T f) { return Vector<T>(a.x()*f, a.y()*f);}
+    template<typename T> Vector<T> operator*(T f, const Vector<T> &a) { return Vector<T>(a.x()*f, a.y()*f);}
+
+    template<typename T> Vector<T> operator/(const Vector<T> &a, T f) { return Vector<T>(a.x()/f, a.y()/f);}
+    template<typename T> Vector<T> operator/(T f, const Vector<T> &a) { return Vector<T>(a.x()/f, a.y()/f);}
+
+    template<typename T> T operator%(const Vector<T> &a, const Vector<T> &b){ return (a.x()*b.x() + a.y()*b.y()); }
+
+    template<typename T> void Vector<T>::norm() { T m = mag(); tx /= m; ty /= m;}
+    template<typename T> void Vector<T>::setLength(T l){ norm(); tx *= l; ty *= l;}
+    template<typename T> T Vector<T>::mag(){ return sqrt(tx*tx + ty*ty);}
 }
--- src/math/vector.h
+++ src/math/vector.h
@@ -1,13 +1,56 @@
 #ifndef SLC_MATH_VECTOR_H
 #define SLC_MATH_VECTOR_H
 
+#include <cmath>
+
 namespace math
 {
+    template<typename T>
     class Vector
     {
         public:
 
+        Vector();
+        Vector(T _x, T _y);
+        ~Vector();
+
+
+        void operator=(const Vector<T> &a);
+
+        // assignments
+        void operator=(const Vector<T> &a);
+        void operator+=(const Vector<T> &a);
+        void operator-=(const Vector<T> &a);
+        void operator*=(T a);
+        void operator/=(T a);
+
+        void norm();
+        void setLength(T l);
+        T mag();
+
+        T x();
+        T y();
+        void set(T, T);
+
+
+
         private:
+        T tx;
+        T ty;
     };
+    // addition, subtraction and negation
+
+    template<typename T> Vector<T> operator+(const Vector<T> &a, const Vector<T> &b);
+    template<typename T> Vector<T> operator-(const Vector<T> &a, const Vector<T> &b);
+    template<typename T> Vector<T> operator-(const Vector<T> &a);
+    // multiplication
+    template<typename T> Vector<T> operator*(const Vector<T> &a, T f);
+    template<typename T> Vector<T> operator*(T f, const Vector<T> &a);
+    // division
+    template<typename T> Vector<T> operator/(const Vector<T> &a, T f);
+    template<typename T> Vector<T> operator/(T f, const Vector<T> &a);
+    // dot
+    template<typename T> T operator%(const Vector<T> &a, const Vector<T> &b);
+
 }
 #endif
