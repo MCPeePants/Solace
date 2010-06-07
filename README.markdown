6da7889ceb99704539099d0e09aaeb92a5241392
Esa Lakaniemi <esalaka@gmail.com>
2010-06-07 21:28:10 +0300

    Sound module brb :D (Let's try this again...)

--- README.markdown
+++
@@ -1,2 +0,0 @@
-### Solace: Star Requiem ###
-Solace: Star Requiem is a side-scrolling space RPG.
--- src/math/vector_fwd.h
+++
@@ -1,6 +0,0 @@
-#ifndef SLC_MATH_VECTOR_FWD_H
-#define SLC_MATH_VECTOR_FWD_H
-
-#include "vector.h"
-
-#endif
