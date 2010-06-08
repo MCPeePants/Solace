9560c137d1a38b4fbf18d9798d3e9ff32311ecfd
z33ky <1zeeky@gmail.com>
2010-06-08 15:41:21 +0200

    commit before merge

--- src/content/xmSound.h
+++ src/content/xmSound.h
@@ -34,8 +34,7 @@ namespace content{
     const int sampleSize = modSettings.mChannels * (modSettings.mBits / 8);
     std::vector<char> samples;
     const std::vector<char>::size_type guessedSize = modSettings.mFrequency * sampleSize * (ModPlug_GetLength(mod) * 0.001f);
-    samples.reserve(guessedSize);
-    // TODO: I guess datasize the minimum length, but a guess is a guess ;z33ky
+    // TODO: I guess guessedSize the minimum length, but a guess is a guess ;z33ky
     char *sample = new char[guessedSize];
     
     ModPlug_Read(mod, sample, guessedSize);
@@ -53,6 +52,48 @@ namespace content{
     sf::SoundBuffer buffer;
     buffer.LoadFromSamples(reinterpret_cast<const sf::Int16*>(samples.data()), samples.size() / sizeof(sf::Int16), modSettings.mChannels, modSettings.mFrequency);
     return buffer;
+  }
+  
+  class XMStream : public sf::SoundStream
+  {
+  public:
+    static sf::SoundStream open(const std::string &path)
+    {
+      std::ifstream modFile(path.c_str(), std::ifstream::in | std::ifstream::binary);
+
+      modFile.seekg(0, std::ifstream::end);
+      const std::streampos fileSize = modFile.tellg();
+      modFile.seekg(0, std::ifstream::beg);
+
+      char *fileData = new char[fileSize];
+      modFile.readsome(fileData, fileSize);
+
+      modFile.close();
+      
+      ModPlug_Settings modSettings;
+      ModPlug_GetSettings(&modSettings);
+      modSettings.mBits = 16; // TODO: probably could do that at initialization-time ;z33ky
+      ModPlug_SetSettings(&modSettings);
+
+      ModPlugFile *mod = ModPlug_Load(fileData, fileSize);
+      delete[] fileData;
+
+      const int sampleSize = modSettings.mChannels * (modSettings.mBits / 8);
+    }
+    
+  private:
+    bool OnGetData(Chunk& data)
+    {
+      
+    }
+    
+    void OnSeek(float timeOffset)
+    {
+      
+    }
+    
+  private:
+    
   }
 
 }
